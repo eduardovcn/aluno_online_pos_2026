ee5394747c67c50bbec36bb214984c1ff67496f0
Kelson Almeida <kelson.victr@gmail.com>
2026-03-21 14:19:01 -0300

    first commit

---
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -0,0 +1,59 @@
+package br.com.alunoonline.api.service;
+
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    AlunoRepository alunoRepository;
+
+    public void criarAluno(Aluno aluno) {
+        alunoRepository.save(aluno);
+    }
+
+    public List<Aluno> buscarTodosAlunos() {
+        return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> buscarAlunoPorId(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    public void deletarAlunoPorId(Long id) {
+        alunoRepository.deleteById(id);
+    }
+
+    public void atualizarAlunoPorId(Long id, Aluno alunoDoFront) {
+
+        // Pegar o dado atual do BD para depois atualizar
+        Optional<Aluno> alunoDoBanco = buscarAlunoPorId(id);
+
+        // Validar se o aluno existe no BD
+        if (alunoDoBanco.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Aluno não encontrado no Banco no BD"
+            );
+        }
+
+        Aluno alunoParaEditar = alunoDoBanco.get();
+
+        // Alterar cada um dos campos
+        alunoParaEditar.setNomeCompleto(alunoDoFront.getNomeCompleto());
+        alunoParaEditar.setEmail(alunoDoFront.getEmail());
+        alunoParaEditar.setCpf(alunoDoFront.getCpf());
+
+        alunoRepository.save(alunoParaEditar);
+
+    }
+
+}
